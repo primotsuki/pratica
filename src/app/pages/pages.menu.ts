629326f4737e47dd4627f7d370653ace5f195b9c
alan <alanfernando93.am@gmail.com>
2017-12-01 18:47:33 -0400

    -

--- src/app/pages/pages.menu.ts
+++ src/app/pages/pages.menu.ts
@@ -13,6 +13,18 @@ export const PAGES_MENU = [
             order: 0
           }
         }
+      },
+      {
+        path: 'dashboard',
+        data: {
+          menu: {
+            title: 'Registro',
+            icon: 'ion-android-home',
+            selected: false,
+            expanded: false,
+            order: 0
+          }
+        }
       }
     ]
   }
