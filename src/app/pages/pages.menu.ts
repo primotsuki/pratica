b95bc11bc6b07b64b1a9b4c743dd8d0ca220a487
claudio <claudio.cuellar@gmail.com>
2017-12-01 12:17:23 -0400

    Removiendo duplicado

--- src/app/pages/pages.menu.ts
+++ src/app/pages/pages.menu.ts
@@ -13,17 +13,6 @@ export const PAGES_MENU = [
             order: 0
           }
         }
-      }, {
-        path: 'dashboard',
-        data: {
-          menu: {
-            title: 'general.menu.dashboard',
-            icon: 'ion-android-home',
-            selected: false,
-            expanded: false,
-            order: 0
-          }
-        }
       }
     ]
   }
