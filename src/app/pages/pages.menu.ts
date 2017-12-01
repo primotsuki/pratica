d9f2e3539dab68e1f82ab179f39c3df3316c3b90
eric <eric@gmail.com>
2017-12-01 16:24:28 -0400

    nuevo menu redes

--- src/app/pages/pages.menu.ts
+++ src/app/pages/pages.menu.ts
@@ -24,6 +24,17 @@ export const PAGES_MENU = [
             order: 0
           }
         }
+      }, {
+        path: 'dashboard',
+        data: {
+          menu: {
+            title: 'redes',
+            icon: 'ion-android-home',
+            selected: false,
+            expanded: false,
+            order: 0
+          }
+        }
       }
     ]
   }
