800915ec11d76d3e9a027caa8b3da77e4469473b
americo <americosombra@gmail.com>
2017-12-01 16:24:42 -0600

    añadir nuevo menu

--- src/app/pages/pages.menu.ts
+++ src/app/pages/pages.menu.ts
@@ -23,6 +23,22 @@ export const PAGES_MENU = [
             expanded: false,
             order: 0
           }
+
+          
+        }
+      },
+      {
+        path: 'dashboard',
+        data: {
+          menu: {
+            title: 'perfil',
+            icon: 'ion-android-home',
+            selected: false,
+            expanded: false,
+            order: 0
+          }
+
+          
         }
       }
     ]
