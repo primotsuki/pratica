1e4ca057f92eb7de831fcf443eaf3411e146736a
GaryMorgaSergio <morga.gary.jet@gmail.com>
2017-12-04 19:47:53 -0700

    cambios 200

--- src/app/pages/pages.menu.ts
+++ src/app/pages/pages.menu.ts
@@ -51,19 +51,7 @@ export const PAGES_MENU = [
             order: 0
           }
         }
-      },{
-        path: 'dashboard',
-        data: {
-          menu: {
-
-            title: 'herramientas',
-            icon: 'ion-android-home',
-            selected: false,
-            expanded: false,
-            order: 0
-          }
-        }
-      }    
+      }
     ]
   }
 ];
