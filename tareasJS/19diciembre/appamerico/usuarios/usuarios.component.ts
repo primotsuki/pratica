beb91f84106480a3d3f1ab87a8ed6bcd4da07997
americo <americosombra@gmail.com>
2017-12-20 03:03:15 +0800

    tarea angular

---
+++ tareasJS/19diciembre/appamerico/usuarios/usuarios.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from './usuarios.service';
+
+
+@Component({
+  selector: 'app-usuarios',
+  templateUrl: './usuarios.component.html',
+  styleUrls: ['./usuarios.component.css']
+})
+export class UsuariosComponent implements OnInit {
+
+  usuarios:Array<any>;
+  usuario :any;
+  constructor(private usuariosService:UsuariosService) {
+    this.usuario={};
+    this.usuario.nombre="";
+   }
+
+  ngOnInit() {
+
+    //this.usuarios=this.usuariosService.obtenerUsuarios();
+    this.usuariosService.obtenerUsuarios().then((usuarios) => {
+      this.usuarios =JSON.parse(usuarios['_body']);
+      
+    });} 
+   nuevoUsuario(){
+    this.usuariosService.agregarUsuarios(this.usuarios).then((respuesta) => {
+     console.log(respuesta);
+     this.ngOnInit();     
+    }); 
+  }
+  
+
+}
